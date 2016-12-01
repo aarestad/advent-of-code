781de9a5685276cd79f984b2bf42851ec1d23297
Peter Aarestad <aarestad@gmail.com>
2016-12-01 10:06:21 -0600

    python3ifying 25.py

--- 2015/25.py
+++ 2015/25.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 # To continue, please consult the code grid in the manual.  Enter the code at row 2947, column 3029.
 
 def next_code(prev_code):
@@ -18,4 +20,4 @@ while row != 2947 or col != 3029:
 		row -= 1
 		col += 1
 
-print row, col, first_code
+print(row, col, first_code)
