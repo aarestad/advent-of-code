64866cbdf48c7f8f1b5caae6837cfcb5e3977de4
Peter Aarestad <aarestad@gmail.com>
2016-12-08 11:40:22 -0600

    factored out commands into separate methods

--- 2016/2016_8.py
+++ 2016/2016_8.py
@@ -5,56 +5,56 @@ import time
 col_matcher = re.compile(r'x=(\d+) by (\d+)')
 row_matcher = re.compile(r'y=(\d+) by (\d+)')
 
+def rect(cols, rows, screen):
+    for y in range(rows):
+        for x in range(cols):
+            screen[y][x] = True
+
+def rotate_column(col_number, col_shift, screen):
+    old_col = [row[col_number] for row in screen]
+    new_col = [None] * len(old_col)
+
+    i = 0
+    for cell in old_col:
+        new_col[(i + col_shift) % len(new_col)] = old_col[i]
+        i += 1
+
+    i = 0
+    for cell in new_col:
+        screen[i][col_number] = new_col[i]
+        i += 1
+
+def rotate_row(row_number, row_shift, screen):
+    old_row = screen[row_number]
+    new_row = [None] * len(old_row)
+
+    i = 0
+    for cell in old_row:
+        new_row[(i + row_shift) % len(new_row)] = old_row[i]
+        i += 1
+
+    i = 0
+    for cell in new_row:
+        screen[row_number][i] = new_row[i]
+        i += 1
+
 def process_cmd(cmd, screen):
     cmd_toks = cmd.split()
 
     if cmd_toks[0] == 'rect':
         (cols, rows) = [int(x) for x in cmd_toks[1].split('x')]
-
-        for y in range(rows):
-            for x in range(cols):
-                screen[y][x] = True
-
+        rect(cols, rows, screen)
     elif cmd_toks[0] == 'rotate':
         if cmd_toks[1] == 'column':
             col_results = col_matcher.match(' '.join(cmd_toks[2:]))
             col_number = int(col_results.group(1))
             col_shift = int(col_results.group(2))
-
-            old_col = [row[col_number] for row in screen]
-            new_col = [None] * len(old_col)
-
-            i = 0
-
-            for cell in old_col:
-                new_col[(i + col_shift) % len(new_col)] = old_col[i]
-                i += 1
-
-            i = 0
-
-            for cell in new_col:
-                screen[i][col_number] = new_col[i]
-                i += 1
-
+            rotate_column(col_number, col_shift, screen)
         elif cmd_toks[1] == 'row':
             row_results = row_matcher.match(' '.join(cmd_toks[2:]))
             row_number = int(row_results.group(1))
             row_shift = int(row_results.group(2))
-
-            old_row = screen[row_number]
-            new_row = [None] * len(old_row)
-
-            i = 0
-
-            for cell in old_row:
-                new_row[(i + row_shift) % len(new_row)] = old_row[i]
-                i += 1
-
-            i = 0
-
-            for cell in new_row:
-                screen[row_number][i] = new_row[i]
-                i += 1
+            rotate_row(row_number, row_shift, screen)
 
 ROWS = 6
 COLS = 50
