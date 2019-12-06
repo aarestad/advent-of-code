6b7ef4e4924ecfe23ec34f8a77c07418ea1e92e5
Peter Aarestad <peter.aarestad@tempus.com>
2019-12-06 14:46:23 -0600

    improve 2015 day 2 a bit

--- 2015/2.py
+++ 2015/2.py
@@ -3,19 +3,14 @@ total_ribbon_length = 0
 
 with open('input_2.txt') as input:
     for package in input:
-        dims = package.split('x')
-        l, w, h = map(int, dims)
+        sides = sorted(int(s) for s in package.split('x'))
 
-        smallest_side = min(l, w, h)
-        other_sides = [l, w, h]
-        other_sides.remove(smallest_side)
-        second_smallest_side = min(*other_sides)
-        side_1 = l * w
-        side_2 = l * h
-        side_3 = w * h
-        volume = l * w * h
+        side_1 = sides[0] * sides[1]
+        side_2 = sides[0] * sides[2]
+        side_3 = sides[1] * sides[2]
+        volume = sides[0] * sides[1] * sides[2]
         total_area += 2 * (side_1 + side_2 + side_3) + min(side_1, side_2, side_3)
-        total_ribbon_length += 2 * smallest_side + 2 * second_smallest_side + volume
+        total_ribbon_length += 2 * sides[0] + 2 * sides[1] + volume
 
 print(total_area, 'sf of paper')
 print(total_ribbon_length, 'f of ribbon')
