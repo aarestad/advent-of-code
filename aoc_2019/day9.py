cdf7fb49e67d3d1766b1d6082b1df5ed7f4ece77
Peter Aarestad <peter.aarestad@tempus.com>
2019-12-09 13:46:32 -0600

    removed commented out thing

--- aoc_2019/day9.py
+++ aoc_2019/day9.py
@@ -167,5 +167,5 @@ class IntcodeMachine:
 if __name__ == '__main__':
     with open('09_input.txt') as program:
         machine = IntcodeMachine(program.readline().strip())
-    # machine = IntcodeMachine('109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99')
+
     machine.run()
