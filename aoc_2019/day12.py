b80f120170cf5978f4876086dde3f9bb5b26f1ba
Peter Aarestad <peter.aarestad@tempus.com>
2019-12-12 12:12:22 -0600

    part 2! woo

--- aoc_2019/day12.py
+++ aoc_2019/day12.py
@@ -1,5 +1,6 @@
 from itertools import count
 from typing import NamedTuple, Iterable
+from aoc_2019.utils import lcm
 
 
 def spaceship(a, b):
@@ -50,34 +51,44 @@ class Planet:
 
 
 if __name__ == '__main__':
-    # my input:
-    # <x=4, y=12, z=13>
-    # <x=-9, y=14, z=-3>
-    # <x=-7, y=-1, z=2>
-    # <x=-11, y=17, z=-1>
-
     planets = [
-        Planet(Vector(x=-1, y=0, z=2), 'A'),
-        Planet(Vector(x=2, y=-10, z=-7), 'B'),
-        Planet(Vector(x=4, y=-8, z=8), 'C'),
-        Planet(Vector(x=3, y=5, z=-1), 'D'),
+        Planet(Vector(x=4, y=12, z=13), 'A'),
+        Planet(Vector(x=-9, y=14, z=-3), 'B'),
+        Planet(Vector(x=-7, y=-1, z=2), 'C'),
+        Planet(Vector(x=-11, y=17, z=-1), 'D'),
     ]
 
     initial_state = {
-        'A': Planet(Vector(x=-1, y=0, z=2), 'A'),
-        'B': Planet(Vector(x=2, y=-10, z=-7), 'B'),
-        'C': Planet(Vector(x=4, y=-8, z=8), 'C'),
-        'D': Planet(Vector(x=3, y=5, z=-1), 'D'),
+        'A': Planet(Vector(x=4, y=12, z=13), 'A'),
+        'B': Planet(Vector(x=-9, y=14, z=-3), 'B'),
+        'C': Planet(Vector(x=-7, y=-1, z=2), 'C'),
+        'D': Planet(Vector(x=-11, y=17, z=-1), 'D'),
     }
 
+    steps_for_x = 0
+    steps_for_y = 0
+    steps_for_z = 0
+
     for i in count(1):
-        if i % 10000 == 0: print(i)
+        if i % 10000 == 0: print('step {}'.format(i))
 
         for planet in planets:
             planet.time_step_vector(p for p in planets if p != planet)
         for planet in planets:
             planet.time_step_pos()
 
-        if all(p.state_eq(initial_state[p.name]) for p in planets):
-            print(i)
+        if not steps_for_x and all(p.pos.x == initial_state[p.name].pos.x and p.vel.x == initial_state[p.name].vel.x for p in planets):
+            steps_for_x = i
+            print('steps for x: {}'.format(steps_for_x))
+        
+        if not steps_for_y and all(p.pos.y == initial_state[p.name].pos.y and p.vel.y == initial_state[p.name].vel.y for p in planets):
+            steps_for_y = i
+            print('steps for y: {}'.format(steps_for_y))
+            
+        if not steps_for_z and all(p.pos.z == initial_state[p.name].pos.z and p.vel.z == initial_state[p.name].vel.z for p in planets):
+            steps_for_z = i
+            print('steps for z: {}'.format(steps_for_z))
+
+        if all((steps_for_x, steps_for_y, steps_for_z)):
+            print(lcm(steps_for_x, lcm(steps_for_y, steps_for_z)))
             break
