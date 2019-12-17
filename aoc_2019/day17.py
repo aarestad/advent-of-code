7299d830d2a04b2045134b84aac60f7ca4f07ddf
Peter Aarestad <peter.aarestad@tempus.com>
2019-12-17 09:12:31 -0600

    print map

--- aoc_2019/day17.py
+++ aoc_2019/day17.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
     with open(sys.argv[1]) as intcode_program:
         machine = IntcodeMachine(intcode_program.readline().strip())
 
+    # machine.memory[0] = 2
+
     map = []
     current_line = []
 
@@ -15,20 +17,5 @@ if __name__ == '__main__':
         else:
             current_line.append(chr_code)
 
-    alignment_param_sum = 0
-
-    for y, line in enumerate(map):
-        if y == 0 or y == len(map) - 1:
-            continue
-        for x, c in enumerate(line):
-            if x == 0 or x == len(line) - 1:
-                continue
-            pound = ord('#')
-            if c == pound and \
-                    line[x - 1] == pound and \
-                    line[x + 1] == pound and \
-                    map[y - 1][x] == pound and \
-                    map[y + 1][x] == pound:
-                alignment_param_sum += y * x
-
-    print(alignment_param_sum)
+    for line in map:
+        print(''.join(chr(c) for c in line))
