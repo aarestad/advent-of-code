24a0ab5c9490b3f32627952cf3c00c4459709216
Peter Aarestad <peter.aarestad@tempus.com>
2020-12-01 10:05:27 -0600

    day 1: better answer using functional programming

--- aoc_2020/day1.py
+++ aoc_2020/day1.py
@@ -1,3 +1,7 @@
+from itertools import combinations
+from operator import mul
+from functools import reduce
+
 if __name__ == '__main__':
     entries = []
 
@@ -5,9 +9,6 @@ if __name__ == '__main__':
         for entry in entry_file:
             entries.append(int(entry.strip()))
 
-    for idx, val1 in enumerate(entries):
-        for val2 in entries[idx + 1:]:
-            for val3 in entries[idx + 2:]:
-                if val1 + val2 + val3 == 2020:
-                    print(val1 * val2 * val3)
-                    exit()
+    for combo in combinations(entries, 3):
+        if sum(combo) == 2020:
+            print(reduce(mul, combo))
