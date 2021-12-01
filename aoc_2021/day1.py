d8b1138cc71d76486437f581f10791746bbae82e
Peter Aarestad <peter.aarestad@tempus.com>
2021-12-01 09:32:59 -0600

    simplify

--- aoc_2021/day1.py
+++ aoc_2021/day1.py
@@ -1,11 +1,8 @@
-depths = []
-
-for line in open("input/day1.txt"):
-    depths.append(int(line.strip()))
+depths = [int(line.strip()) for line in open("input/day1.txt")]
 
 raw_increases = 0
 window_increases = 0
-prev_values = []
+prev_values = ()
 
 for depth in depths:
     if len(prev_values) > 0 and depth > prev_values[-1]:
@@ -14,7 +11,7 @@ for depth in depths:
     if len(prev_values) == 3 and sum(prev_values[1:]) + depth > sum(prev_values):
         window_increases += 1
 
-    prev_values = (prev_values if len(prev_values) < 3 else prev_values[1:]) + [depth]
+    prev_values = (prev_values if len(prev_values) < 3 else prev_values[1:]) + (depth,)
 
 print(raw_increases)
 print(window_increases)
