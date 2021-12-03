c10e896165c0f96446d701088a57b4fbfecc1799
Peter Aarestad <peter.aarestad@tempus.com>
2021-12-03 10:36:19 -0600

    python 3-ify

--- 2015/5_2.py
+++ 2015/5_2.py
@@ -1,11 +1,11 @@
 import re
 
-with open('input_5.txt') as input:
-  nice_string_count = 0
-  for line in input:
-    if re.search('(..).*\\1', line) and re.search('(.).\\1', line):
-      nice_string_count += 1
-    else:
-      print('naughty line:', line)
+with open("input_5.txt") as input:
+    nice_string_count = 0
+    for line in input:
+        if re.search("(..).*\\1", line) and re.search("(.).\\1", line):
+            nice_string_count += 1
+        else:
+            print("naughty line:", line)
 
-  print nice_string_count
+    print(nice_string_count)
