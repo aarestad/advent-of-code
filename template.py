f3f982c16c9787da1c3a28b6084b02e801e913e1
Peter Aarestad <peter.aarestad@tempus.com>
2021-12-06 09:32:38 -0600

    update template

--- template.py
+++ template.py
@@ -1,9 +1,7 @@
-example = """"""
-
-example_input = example.split("\n")
-
-with open("input/dayXX.txt") as input:
-    problem_input = [i.strip() for i in input.readlines()]
-
 if __name__ == "__main__":
-    pass
+    example = """"""
+
+    example_input = example.split("\n")
+
+    with open("input/dayXX.txt") as input:
+        problem_input = [i.strip() for i in input.readlines()]
