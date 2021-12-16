e0fa9d7a24f0fdcdeabcb635496bbabf913ebc1a
Peter Aarestad <peter.aarestad@tempus.com>
2021-12-16 14:29:47 -0600

    argh

--- aoc_2021/day16.py
+++ aoc_2021/day16.py
@@ -1,3 +1,6 @@
+import math
+
+
 def binary_list_to_int(binary_list: list[int]) -> int:
     result = 0
 
@@ -7,48 +10,94 @@ def binary_list_to_int(binary_list: list[int]) -> int:
     return result
 
 
-def parse_message(binary_list: list[int]) -> (int, list[int]):
-    """Parse a packet from the front of a binary_list. Return the result of this packet and the rest of the unparsed list"""
-    value = 0
+def get_subpackets(binary_list: list[int]) -> list[list[int]]:
+    length_type = binary_list[0]
 
-    packet_version = binary_list_to_int(binary_list[0:3])
-    packet_type = binary_list_to_int(binary_list[3:6])
-    value += packet_version
+    if length_type:
+        num_subpackets = binary_list_to_int(binary_list[1:12])
+        rest_of_packet = binary_list[13:]
+    else:
+        subpacket_length = binary_list_to_int(binary_list[1:16])
+        rest_of_packet = binary_list[16:]
 
-    match packet_type:
+    return []
+
+
+def parse_packet(binary_list: list[int]) -> int:
+    match packet_type := binary_list_to_int(binary_list[3:6]):
+        case 0:
+            print("sum packet")
+            value = sum(
+                parse_packet(packet) for packet in get_subpackets(binary_list[6:])
+            )
+        case 1:
+            print("product packet")
+            value = math.prod(
+                parse_packet(packet) for packet in get_subpackets(binary_list[6:])
+            )
+        case 2:
+            print("min packet")
+            value = min(
+                parse_packet(packet) for packet in get_subpackets(binary_list[6:])
+            )
+        case 3:
+            print("max packet")
+            value = max(
+                parse_packet(packet) for packet in get_subpackets(binary_list[6:])
+            )
         case 4:
+            print("literal packet")
             literal_value_binary_list = []
             current_start = 6
             not_last_group = 1
 
             while not_last_group:
-                chunk = binary_list[current_start:current_start+5]
+                chunk = binary_list[current_start : current_start + 5]
                 not_last_group = chunk[0]
                 literal_value_binary_list.extend(chunk[1:])
                 current_start += 5
 
-            print(f"literal value: {binary_list_to_int(literal_value_binary_list)}")
-            rest_of_message = binary_list[current_start:]
+            literal_value = binary_list_to_int(literal_value_binary_list)
+            print(f"literal value: {literal_value}")
+            value = literal_value
+        case 5:
+            print("gt packet")
+            subpackets = get_subpackets(binary_list[6:])
+
+            if len(subpackets) != 2:
+                raise ValueError("gt packet has != 2 subpackets")
+
+            left_value = parse_packet(subpackets[0])
+            right_value = parse_packet(subpackets[1])
+            value = 1 if left_value > right_value else 0
+        case 6:
+            print("lt packet")
+            subpackets = get_subpackets(binary_list[6:])
+
+            if len(subpackets) != 2:
+                raise ValueError("lt packet has != 2 subpackets")
+
+            left_value = parse_packet(subpackets[0])
+            right_value = parse_packet(subpackets[1])
+            value = 1 if left_value < right_value else 0
+        case 7:
+            print("eq packet")
+            subpackets = get_subpackets(binary_list[6:])
+
+            if len(subpackets) != 2:
+                raise ValueError("eq packet has != 2 subpackets")
+
+            left_value = parse_packet(subpackets[0])
+            right_value = parse_packet(subpackets[1])
+            value = 1 if left_value == right_value else 0
         case _:
-            length_type = binary_list[6]
+            raise ValueError(f"bad packet type: {packet_type}")
 
-            if length_type:
-                num_subpackets = binary_list_to_int(binary_list[7:18])
-                rest_of_message = binary_list[18:]
-            else:
-                total_length = binary_list_to_int(binary_list[7:22])
-                rest_of_message = binary_list[22:]
-
-    if len(rest_of_message) >= 8 or not all(d == 0 for d in rest_of_message):
-        while not all(d == 0 for d in rest_of_message):
-            packet_value, rest_of_message = parse_message(rest_of_message)
-            value += packet_value
-
-    return value, rest_of_message
+    return value
 
 
 if __name__ == "__main__":
-    example = """9C0141080250320F1802104A08"""
+    example = """D8005AC2A8F0"""
 
     example_input = example.split("\n")
 
@@ -63,4 +112,4 @@ if __name__ == "__main__":
         for n in range(3, -1, -1):
             decoded_bits.append(1 if (1 << n) & d else 0)
 
-    print(parse_message(decoded_bits)[0])
+    print(parse_packet(decoded_bits))
