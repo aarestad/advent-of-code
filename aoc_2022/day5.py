ce72dc25983dd0bf79f760994a050e386b8a76e8
Peter Aarestad <aaretad@gmail.com>
2022-12-05 10:38:40 -0600

    2022 day 5 part 2

--- aoc_2022/day5.py
+++ aoc_2022/day5.py
@@ -25,7 +25,9 @@ move 1 from 1 to 2"""
     for _ in range(num_stacks):
         stacks.append([])
 
-    stack_lines = actual_input[0:8]
+    highest_stack = 8  # barf :)
+
+    stack_lines = actual_input[0:highest_stack]
 
     for line in stack_lines:
         for s in range(len(stacks)):
@@ -33,9 +35,12 @@ move 1 from 1 to 2"""
             if crate != " ":
                 stacks[s].append(crate)
 
-    print(stacks)
+    # deep copy for part 2
+    part_2_stacks = []
+    for s in stacks:
+        part_2_stacks.append(s[:])
 
-    instructions = actual_input[10:]
+    instructions = actual_input[highest_stack + 2 :]
     instruction_matcher = re.compile(r"move (\d+) from (\d) to (\d)")
 
     for line in instructions:
@@ -46,10 +51,14 @@ move 1 from 1 to 2"""
         (num_to_move, target_stack, dest_stack) = (
             int(g) for g in instruction_match.groups()
         )
-        print((num_to_move, target_stack, dest_stack))
 
         for _ in range(num_to_move):
             crate = stacks[target_stack - 1].pop(0)
             stacks[dest_stack - 1].insert(0, crate)
 
-    print("".join(s[0] for s in stacks))
+        chunk_to_move = part_2_stacks[target_stack - 1][0:num_to_move]
+        del part_2_stacks[target_stack - 1][0:num_to_move]
+        part_2_stacks[dest_stack - 1] = chunk_to_move + part_2_stacks[dest_stack - 1]
+
+    print("part 1:", "".join(s[0] for s in stacks))
+    print("part 2:", "".join(s[0] for s in part_2_stacks))
