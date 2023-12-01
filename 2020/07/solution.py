ee7fbf97a2b28dc00a9d4ab72b4f3d7320912409
Peter Aarestad <aarestad@gmail.com>
2023-12-01 15:48:18 -0600

    skeletons for previous years

---
+++ 2020/07/solution.py
@@ -0,0 +1,12 @@
+## advent of code 2020
+## https://adventofcode.com/2020
+## day 07
+
+def parse_input(lines):
+    pass
+
+def part1(data):
+    pass
+
+def part2(data):
+    pass
---
+++ 2021/22/solution.py
@@ -0,0 +1,12 @@
+## advent of code 2021
+## https://adventofcode.com/2021
+## day 22
+
+def parse_input(lines):
+    pass
+
+def part1(data):
+    pass
+
+def part2(data):
+    pass
