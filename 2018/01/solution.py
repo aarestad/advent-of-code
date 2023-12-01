da5fd0c335ebb7ceaeff6cea65302078dbbb3ee2
Peter Aarestad <aarestad@gmail.com>
2023-12-01 15:39:11 -0600

    2018 day 1

--- 2018/01/solution.py
+++ 2018/01/solution.py
@@ -2,11 +2,22 @@
 ## https://adventofcode.com/2018
 ## day 01
 
+
 def parse_input(lines):
-    pass
+    return [int(l) for l in lines]
+
 
 def part1(data):
-    pass
+    return sum(data)
+
 
 def part2(data):
-    pass
+    current_freq = 0
+    seen_frequencies = {0}
+
+    while True:
+        for delta in data:
+            current_freq += delta
+            if current_freq in seen_frequencies:
+                return current_freq
+            seen_frequencies.add(current_freq)
