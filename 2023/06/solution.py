6cf9d472a2a781fbba0f370b9819eb21223b0d19
Peter Aarestad <aarestad@gmail.com>
2023-12-05 23:13:43 -0600

    2023 day 6 (naive, but still fast)

---
+++ 2023/06/solution.py
@@ -0,0 +1,42 @@
+## advent of code 2023
+## https://adventofcode.com/2023
+## day 06
+
+from dataclasses import dataclass
+from functools import reduce
+from operator import mul
+
+
+@dataclass
+class Race:
+    time: int
+    distance: int
+
+    def num_ways_to_win(self) -> int:
+        num_ways = 0
+
+        for t in range(1, self.time):
+            if t * (self.time - t) > self.distance:
+                num_ways += 1
+
+        return num_ways
+
+
+def parse_input(lines):
+    times = [int(t) for t in lines[0].split()[1:]]
+    distances = [int(d) for d in lines[1].split()[1:]]
+
+    part_2_time = int("".join(lines[0].split()[1:]))
+    part_2_distance = int("".join(lines[1].split()[1:]))
+
+    races = [Race(t, d) for t, d in zip(times, distances)]
+    races.append(Race(part_2_time, part_2_distance))
+    return races
+
+
+def part1(races):
+    return reduce(mul, (r.num_ways_to_win() for r in races[:-1]))
+
+
+def part2(races):
+    return races[-1].num_ways_to_win()
