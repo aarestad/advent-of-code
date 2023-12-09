01e4d34f023e49c9e7583d0a0518cc04a134e1d9
Peter Aarestad <aarestad@gmail.com>
2023-12-08 23:28:54 -0600

    cleanup

--- 2023/09/solution.py
+++ 2023/09/solution.py
@@ -7,20 +7,23 @@ def parse_input(lines):
     return [[int(n) for n in l.split()] for l in lines]
 
 
-def extrapolate(data) -> int:
+def extrapolate_next(data) -> int:
     if all(d == 0 for d in data):
-        extrapolated = 0
-    else:
-        extrapolated = data[-1] + extrapolate(
-            [n2 - n1 for n1, n2 in zip(data, data[1:])]
-        )
+        return 0
 
-    return extrapolated
+    return data[-1] + extrapolate_next([n2 - n1 for n1, n2 in zip(data, data[1:])])
+
+
+def extrapolate_previous(data) -> int:
+    if all(d == 0 for d in data):
+        return 0
+
+    return data[0] - extrapolate_previous([n2 - n1 for n1, n2 in zip(data, data[1:])])
 
 
 def part1(data):
-    return sum(extrapolate(d) for d in data)
+    return sum(extrapolate_next(d) for d in data)
 
 
 def part2(data):
-    pass
+    return sum(extrapolate_previous(d) for d in data)
