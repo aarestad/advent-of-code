594967bd788c56a5cd559b13e94c9fd8b2d18d73
Peter Aarestad <aarestad@gmail.com>
2023-12-15 16:02:37 -0600

    2023 day 14 part 1

--- 2023/14/solution.py
+++ 2023/14/solution.py
@@ -2,11 +2,53 @@
 ## https://adventofcode.com/2023
 ## day 14
 
-def parse_input(lines):
-    pass
+from enum import StrEnum
 
-def part1(data):
-    pass
+
+class PlatformTile(StrEnum):
+    ROCK = "O"
+    CUBE = "#"
+    EMPTY = "."
+
+
+def parse_input(lines):
+    platform_lines: [[PlatformTile]] = []
+
+    for line in lines:
+        if not line:
+            continue
+
+        platform_lines.append([PlatformTile(c) for c in line])
+
+    return platform_lines
+
+
+def part1(platform_lines):
+    for row, line in enumerate(platform_lines):
+        for col, tile in enumerate(line):
+            if tile != PlatformTile.ROCK:
+                continue
+
+            line[col] = PlatformTile.EMPTY
+
+            for row_above in reversed(range(row)):
+                if platform_lines[row_above][col] in (
+                    PlatformTile.ROCK,
+                    PlatformTile.CUBE,
+                ):
+                    # may just be replacing the rock in its current place
+                    platform_lines[row_above + 1][col] = PlatformTile.ROCK
+                    break
+            else:  # it rolled to the top
+                platform_lines[0][col] = PlatformTile.ROCK
+
+    return sum(
+        len(platform_lines) - row
+        for row, line in enumerate(platform_lines)
+        for tile in line
+        if tile == PlatformTile.ROCK
+    )
+
 
 def part2(data):
     pass
