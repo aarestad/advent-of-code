e9d25027dde9e94570132820a432172ee9590c3c
Peter Aarestad <aarestad@gmail.com>
2023-12-15 09:35:51 -0600

    2023 day 15 part 1

--- 2023/15/solution.py
+++ 2023/15/solution.py
@@ -2,11 +2,25 @@
 ## https://adventofcode.com/2023
 ## day 15
 
-def parse_input(lines):
-    pass
 
-def part1(data):
-    pass
+def parse_input(lines):
+    return lines[0].split(",")
+
+
+def hash_string(s) -> int:
+    hash = 0
+
+    for c in s:
+        hash += ord(c)
+        hash *= 17
+        hash %= 256
+
+    return hash
+
+
+def part1(init_steps):
+    return sum(hash_string(s) for s in init_steps)
+
 
 def part2(data):
     pass
