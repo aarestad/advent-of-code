eeadd44ebb39a2da422caab5976c5dfeee374fb0
Peter Aarestad <peter@landscapehub.com>
2024-11-30 21:27:29 -0600

    2024 Day 1 init commit

---
+++ 2024/aoc_2024/day1.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    example = """"""
+
+    example_input = example.split("\n")
+
+    with open("input/day1.txt") as input:
+        problem_input = [i.strip() for i in input.readlines()]
