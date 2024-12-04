857db25f0182ad0ff8ec49becad9aad5ab69bb03
Peter Aarestad <peter@landscapehub.com>
2024-12-04 12:35:57 -0600

    2024 day 4

---
+++ 2024/aoc_2024/day4.py
@@ -0,0 +1,149 @@
+def xmas_up(input, row, col):
+    if row < 3:
+        return False
+
+    return (
+        input[row][col] == "X"
+        and input[row - 1][col] == "M"
+        and input[row - 2][col] == "A"
+        and input[row - 3][col] == "S"
+    )
+
+
+def xmas_down(input, row, col):
+    if row > len(input) - 4:
+        return False
+
+    return (
+        input[row][col] == "X"
+        and input[row + 1][col] == "M"
+        and input[row + 2][col] == "A"
+        and input[row + 3][col] == "S"
+    )
+
+
+def xmas_left(input, row, col):
+    if col < 3:
+        return False
+
+    return (
+        input[row][col::-1] if col == 3 else input[row][col : col - 4 : -1]
+    ) == "XMAS"
+
+
+def xmas_right(input, row, col):
+    if col > len(input[row]) - 4:
+        return False
+
+    return input[row][col : col + 4] == "XMAS"
+
+
+def xmas_nw(input, row, col):
+    if row < 3 or col < 3:
+        return False
+
+    return (
+        input[row][col] == "X"
+        and input[row - 1][col - 1] == "M"
+        and input[row - 2][col - 2] == "A"
+        and input[row - 3][col - 3] == "S"
+    )
+
+
+def xmas_ne(input, row, col):
+    if row < 3 or col > len(input[row]) - 4:
+        return False
+
+    return (
+        input[row][col] == "X"
+        and input[row - 1][col + 1] == "M"
+        and input[row - 2][col + 2] == "A"
+        and input[row - 3][col + 3] == "S"
+    )
+
+
+def xmas_sw(input, row, col):
+    if row > len(input) - 4 or col < 3:
+        return False
+
+    return (
+        input[row][col] == "X"
+        and input[row + 1][col - 1] == "M"
+        and input[row + 2][col - 2] == "A"
+        and input[row + 3][col - 3] == "S"
+    )
+
+
+def xmas_se(input, row, col):
+    if row > len(input) - 4 or col > len(input[row]) - 4:
+        return False
+
+    return (
+        input[row][col] == "X"
+        and input[row + 1][col + 1] == "M"
+        and input[row + 2][col + 2] == "A"
+        and input[row + 3][col + 3] == "S"
+    )
+
+
+def x_mas(input, row, col):
+    if row < 1 or row > len(input) - 2 or col < 1 or col > len(input[row]) - 2:
+        return False
+
+    nw = input[row + 1][col + 1] + input[row][col] + input[row - 1][col - 1]
+    ne = input[row + 1][col - 1] + input[row][col] + input[row - 1][col + 1]
+
+    return nw in ["MAS", "SAM"] and ne in ["MAS", "SAM"]
+
+
+if __name__ == "__main__":
+    example = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"""
+
+    example_input = example.split("\n")
+
+    with open("input/day4.txt") as input:
+        problem_input = [i.strip() for i in input.readlines()]
+
+    word_search = problem_input
+
+    input_width = len(word_search[0])
+
+    xmases = 0
+
+    for row in range(len(word_search)):
+        for col in range(input_width):
+            if word_search[row][col] == "X":
+                xmases += sum(
+                    1
+                    for dir in (
+                        xmas_up,
+                        xmas_down,
+                        xmas_left,
+                        xmas_right,
+                        xmas_ne,
+                        xmas_nw,
+                        xmas_se,
+                        xmas_sw,
+                    )
+                    if dir(word_search, row, col)
+                )
+
+    print(f"{xmases} XMASes")
+
+    x_mases = 0
+
+    for row in range(len(word_search)):
+        for col in range(input_width):
+            if x_mas(word_search, row, col):
+                x_mases += 1
+
+    print(f"{x_mases} X-MASes")
