28a06c05ad56803541f641f516cf243a3e10939f
Peter Aarestad <peter@landscapehub.com>
2024-12-04 12:05:07 -0600

    2024 day 3 part 2

--- 2024/aoc_2024/day3.py
+++ 2024/aoc_2024/day3.py
@@ -1,8 +1,10 @@
 import re
 
-PART_1_MUL = re.compile(
+MUL = re.compile(
     r"mul\((\d{1,3}),(\d{1,3})\)",
 )
+
+DO_OR_DONT_OR_MUL = re.compile(r"(don't\(\)|do\(\)|mul\((\d{1,3}),(\d{1,3})\))")
 
 if __name__ == "__main__":
     example = (
@@ -12,4 +14,19 @@ if __name__ == "__main__":
     with open("input/day3.txt") as input:
         problem_input = "".join(i.strip() for i in input.readlines())
 
-    print(sum(int(m[1]) * int(m[2]) for m in PART_1_MUL.finditer(problem_input)))
+    print(f"part 1: {sum(int(m[1]) * int(m[2]) for m in MUL.finditer(problem_input))}")
+
+    do_mult = True
+    part_2_mul_sum = 0
+
+    for m in DO_OR_DONT_OR_MUL.finditer(problem_input):
+        match = m[1]
+
+        if match == "don't()":
+            do_mult = False
+        elif match == "do()":
+            do_mult = True
+        elif do_mult:  # mul
+            part_2_mul_sum += int(m[2]) * int(m[3])
+
+    print(f"part 2: {part_2_mul_sum}")
